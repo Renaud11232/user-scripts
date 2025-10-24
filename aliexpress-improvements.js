d0be9a4114921227e556821c58a55fda361a05e0
Renaud GASPARD <gaspardrenaud@hotmail.com>
2025-10-25 00:13:21 +0200

    Update version and improve CSS for AliExpress script

--- aliexpress-improvements.js
+++ aliexpress-improvements.js
@@ -1,25 +1,18 @@
 // ==UserScript==
 // @name         AliExpress Improvements
 // @namespace    https://renaud11232.github.io
-// @version      1.0.0
+// @version      1.1.0
 // @description  Collections of improvements to AliExpress
 // @author       Renaud11232
-// @match        http*://*.aliexpress.com/w/*
+// @match        http*://*.aliexpress.com/*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=fr.aliexpress.com
-// @grant        none
+// @grant        GM_addStyle
 // ==/UserScript==
 
 (function() {
-    new MutationObserver(() => {
-        [...document.querySelectorAll(".search-card-item")].forEach(card => {
-            if(card.href.match(/^https?:\/\/.*?\.aliexpress\.com\/ssr\/.*?$/)) {
-                card.style.opacity = 0.2;
-            } else {
-                card.style.opacity = 1;
-            }
-        });
-    }).observe(document.getElementById("card-list"), {
-        childList: true,
-        subtree: true
-    });
+    GM_addStyle(`
+        a[href*=".aliexpress.com/ssr/"] {
+            opacity: 20%;
+        }
+    `);
 })();
